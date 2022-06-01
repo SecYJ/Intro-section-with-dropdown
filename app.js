38a1c8f5f308305b4cfbf1dd834d76279462a944
QiaN HuNTeR <qianqian1903@gmail.com>
2022-06-02 01:25:52 +0800

    completed

---
+++ app.js
@@ -0,0 +1,34 @@
+const btnMenu = document.querySelector("[data-menu-btn]");
+const btnClose = document.querySelector("[data-btn-close]");
+const overlay = document.querySelector(".overlay");
+const sidebar = document.querySelector("[data-sidebar]");
+const dropdownButtons = document.querySelectorAll("[data-dropdown-button");
+const dropdownMobileButtons = document.querySelectorAll(
+	"[data-mobile-dropdown-btn]"
+);
+
+console.log(dropdownMobileButtons);
+
+btnMenu.addEventListener("click", () => {
+	overlay.classList.add("active");
+	sidebar.classList.add("show");
+});
+
+btnClose.addEventListener("click", () => {
+	overlay.classList.remove("active");
+	sidebar.classList.remove("show");
+});
+
+dropdownButtons.forEach((dropdown) => {
+	dropdown.addEventListener("click", () => {
+		const parentEl = dropdown.closest(".nav-item");
+		parentEl.classList.toggle("open");
+	});
+});
+
+dropdownMobileButtons.forEach((dropdown) => {
+	dropdown.addEventListener("click", () => {
+		const parentEl = dropdown.closest(".nav-list");
+		parentEl.classList.toggle("open");
+	});
+});
